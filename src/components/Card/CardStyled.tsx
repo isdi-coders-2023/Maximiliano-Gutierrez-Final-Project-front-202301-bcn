4bbf1d686ee054c91d2ff405ecc62349a4f6a394
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-03-22 11:47:46 +0100

    Improve accesibility in Lighthouse and performance

--- src/components/Card/CardStyled.tsx
+++ src/components/Card/CardStyled.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const CardStyled = styled.div`
+const CardStyled = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: center;
